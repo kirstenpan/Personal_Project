5008c08e40781e362dc6e866fe6d92ca230f86e3
kirstenpan <96449839+kirstenpan@users.noreply.github.com>
2022-08-03 18:13:21 -0400

    update

--- 220615_topic_extractor/file_0.py
+++ 220615_topic_extractor/file_0.py
@@ -56,4 +56,4 @@ PATH = ''
 on_vm = False
 if socket.gethostname().find('ec2') != -1:
     on_vm = True
-    PATH = 'civilience/2206_internship_program/220615_scraping_pipeline/'
+    PATH = 'personal_project/topic_modeling/'
